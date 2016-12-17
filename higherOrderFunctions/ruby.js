e20eef2658933e6867d50b414e174e723ffa2b90
Shahrukh Zindani <szindani@nectarom.com>
2016-12-17 03:25:23 -0600

    3rd in the series.

---
+++ higherOrderFunctions/ruby.js
@@ -0,0 +1,14 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if at least one Ruby developer has signed up; or
+// false if there will be no Ruby developers.
+
+function isRubyComing(list) {
+  return list.some(isRuby);
+}
+
+function isRuby(x) {
+  return x.language=='Ruby'
+}

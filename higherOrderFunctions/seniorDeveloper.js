3feacaf92efb2e03dd4abc3c40f38a73a3bebe9f
Shahrukh Zindani <szindani@nectarom.com>
2016-12-17 14:51:10 -0600

    7th in the series

---
+++ higherOrderFunctions/seniorDeveloper.js
@@ -0,0 +1,27 @@
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an array which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+//   { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+//   { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+// ]
+function maxReturn(numArray){
+  var senior = Math.max.apply(null, numArray.map(function(x) {
+    return x.age;
+  }));
+  return list1.filter(function(x) {
+    return x.age == senior
+  }) 
+}
+maxReturn(list1)

b3cff66bd30849d270707dd6e36f7c243189a556
Shahrukh Zindani <szindani@nectarom.com>
2016-12-17 03:59:56 -0600

    Add 5th in the series

---
+++ higherOrderFunctions/countLanguage.js
@@ -0,0 +1,25 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an object (associative array in PHP) which includes the count of each coding language represented at the meetup.
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+//   { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+// ];
+// your function should return the following object (associative array in PHP):
+
+// { C: 2, JavaScript: 1, Ruby: 1 }
+function countLanguages(list1) {
+  return list1.reduce(function(object, element) {
+    if(element.language in object) 
+      object[element.language]++;
+    else
+      object[element.language] = 1;
+    
+    return object;
+  }, {});
+}

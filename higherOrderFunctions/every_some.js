fb503ba35926013b6f5412911a6cbae003266225
Shahrukh Zindani <szindani@nectarom.com>
2016-12-17 22:34:36 -0600

    8th in a series.

---
+++ higherOrderFunctions/every_some.js
@@ -0,0 +1,31 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return:
+
+// true if all of the following continents / geographic zones will be represented by at least one developer: 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'.
+// false otherwise.
+// For example, given the following input array:
+'use strict'
+var list1 = [
+  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+];
+var list2 = [
+  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Afri', age: 25, language: 'JavaScript' },
+  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+];
+// your function should return true as there is at least one developer from the required 5 geographic zones.
+function allContinents(list) {
+  return ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'].every(function(x) {
+    return list.some(function(y) {
+      return x === y.continent
+    })
+  })
+}
+

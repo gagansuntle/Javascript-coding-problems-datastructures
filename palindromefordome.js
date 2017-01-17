b903391bf8d3ee9635c99ed7f4559416a1d6cb42
Shahrukh Zindani <shahrukh.zindani@gmail.com>
2017-01-17 01:16:57 -0600

    one more

---
+++ palindromefordome.js
@@ -0,0 +1,25 @@
+// A palindrome is a word, phrase, number, or other sequence of symbols or elements, whose meaning may be interpreted the same way in either forward or reverse direction. Famous examples include "Amore, Roma", "A man, a plan, a canal: Panama" and "No 'x' in 'Nixon'". - wikipedia
+
+// Our goal is to determine whether or not a given string is a valid palindrome or not.
+
+// Like the above examples, here are a few test cases which are also populated:
+
+// "Amore, Roma" => valid
+// "A man, a plan, a canal: Panama" => valid
+// "No 'x' in 'Nixon'" => valid
+// "Abba Zabba, you're my only friend" => invalid
+
+function palindrome(string) {
+  
+  orignal = string.replace(/[^A-Za-z]/g, "").toLowerCase();
+  string = orignal.split('');
+  var newString = [];
+  for(var i=string.length-1; i>=0; i--) {
+    newString.push(string[i])
+  }
+  newString = newString.join('')
+  if(orignal===newString)
+    return true
+  else
+    return false
+}
